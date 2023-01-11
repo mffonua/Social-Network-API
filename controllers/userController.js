cdab04ff527814d9df34feb20f7224de4bfa5d9d
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-10 19:59:14 -0700

    updated the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,26 +9,86 @@ module.exports = {
     },
     // create user
     createUser(req, res) {
-
+        User.create(req.body)
+        .then(user(user => res.json(user)))
+        .catch((err) => res.status(500).json(err));
     },
     // get single user
     getSingleUser(req, res) {
-
+        User.findOne({ _id: req.params.userId })
+        .then((user) => {
+            if(!user) {
+                res.status(404).json({message: 'No user found with this ID'})
+            } else {
+                res.json(user)
+            };
+        })
+        .catch((err) => res.status(500).json(err));
     },
     // update a single user
     updateUser(req, res) {
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body},
+            { runValidators: true, new: true})
+            .then((user) => {
+            if(!user) {
+                res.status(404).json({ message: 'No user with this id found'})
+            } else {
+                res.json(user)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
     },
     // delete a single user
     deleteUser(req, res) {
-
+        User.findOneAndDelete(
+            { _id: req.params.userId })
+            .then((user) => {
+                if(!user) {
+                    res.status(404).json({ message: 'No user found with this id'})
+                } else {
+                    res.json(user)
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     },
     // add a user to friend list
     addFriend(req, res) {
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: {friends: { _id: req.params.friendId }}},
+            { runValidators: true, new: true})
+            .then((user) => {
+                if(!user) {
+                    res.status(404).json({ message: 'No user found with this id'})
+                } else {
+                    res.json(user)
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     },
     // delete a user from friend list
     deleteFriend(req, res) {
-
-    }
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId }}
+        )
+        .then((user) => {
+            if(!user) {
+                res.status(404).json({ message: 'No user with this id' })
+            } else {
+                res.json(user)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+    }, 
 };
